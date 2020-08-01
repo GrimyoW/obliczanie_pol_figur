0d3f22ce81ed715a05aaf8c217eea89762a41681
Maciek-prog <pretkimaciej1@gmail.com>
2020-08-01 13:17:27 +0200

    Abstract

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         Square square = new Square("kwadrat", 5);
-        System.out.println(square.getField());
         Rectangle rectangle = new Rectangle("prostokat", 5, 6);
-        System.out.println(rectangle.getField());
+//        System.out.println(rectangle.getField());
+//        System.out.println(square.getField());
 
         Figura[] figury = {square,rectangle};
         for (Figura figura: figury) {
